a3c5a8dd060530ed8c9c0836445f9cc9fae686dc
Markus <48181660+BackInBash@users.noreply.github.com>
2021-11-05 04:23:25 +0100

    Added Client

---
+++ ChatClient/src/main/java/de/edv/chatclient/Client.java
@@ -0,0 +1,88 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2021 Markus.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.edv.chatclient;
+
+import com.google.protobuf.Any;
+import de.protobuf.edv.ChatProtocol.ChatMessage;
+import de.protobuf.edv.ChatProtocol.OnlineUsers;
+import de.protobuf.edv.ChatProtocol.User;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Markus
+ */
+public class Client {
+    private String ip;
+    private int port = 2048;
+    private Socket socket;
+    private User user;
+
+    public Client(String ip, int port, User user) {
+        this.ip = ip;
+        this.port = port;
+        this.user = user;
+    }
+
+    public OnlineUsers getOnlineUsers() throws IOException{
+        OutputStream output = socket.getOutputStream();
+        OnlineUsers ou = OnlineUsers.newBuilder().setUsers(0, User.newBuilder().setColor("").setUsername("").setStatus(User.Status.ONLINE).build()).build();
+        
+        // Send Request
+        output.write(ou.toByteArray());
+        
+        // Get Response
+        InputStream input = socket.getInputStream();
+        Any data = Any.parseFrom(input.readAllBytes());
+        if (data.is(OnlineUsers.class)){
+            return data.unpack(OnlineUsers.class);
+        }
+        return null;
+    }
+    
+    public void startMessageThread(javax.swing.JTextArea chat){
+        new ChatThread(this.socket, chat).start();
+    }
+    
+    public void sendMessage(String message) throws IOException{
+        OutputStream output = socket.getOutputStream();
+        ChatMessage msg = ChatMessage.newBuilder().setArrivalTime(System.currentTimeMillis()).setFromUser(user).setMsg(message).setType(ChatMessage.Type.SEND).build();
+        // Send Request
+        output.write(msg.toByteArray());
+    }
+    
+    public void connect() {
+        try {
+            socket = new Socket(ip, port);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
